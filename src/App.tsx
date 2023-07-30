987cd4176d575fa2f028f627884e391e8a2bc474
Pawan Udassi <pawanudassi@hotmail.com>
2023-07-30 13:35:58 +0530

    centralised paritially ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,87 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import Create from "./pages/Create";
-import Stake from "./pages/Stake";
-import Exchange from "./pages/Exchange";
-import Loan from "./pages/Loan";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Create from "./pages/Decentralised/Create";
+import Stake from "./pages/Decentralised/Stake";
+import Exchange from "./pages/Decentralised/Exchange";
+import Loan from "./pages/Decentralised/Loan";
 import { ThemeProvider } from "@mui/material/styles";
 import mytheme from "./MuiTheme";
-import Test from "./pages/Test";
-import Layout from "./components/Layout";
-import CollateralNfts from "./pages/CollateralNfts";
+import Test from "./pages/Decentralised/Test";
+import LayoutDecentralised from "./components/LayoutDecentralised";
+import LayoutCentralised from "./components/LayoutCentralised";
+import CollateralNfts from "./pages/Decentralised/CollateralNfts";
+import CollateralNftsCentralised from "./pages/Centralised/CollateralNfts";
+import Start from "./pages/Start";
+import GetJwt from "./pages/Centralised/GetJwt";
+import SubmitKyc from "./pages/Centralised/SubmitKyc";
+import CreateCentralised from "./pages/Centralised/Create";
+import StakeCentralised from "./pages/Centralised/Stake";
+import ExchangeCentralised from "./pages/Centralised/Exchange";
+import LoanCentralised from "./pages/Centralised/Loan";
+import centerTheme from "./MuiThemeCentralised";
+
 function App() {
   return (
     <ThemeProvider theme={mytheme}>
       <Router>
-        <Layout>
-          <Switch>
-            <Route path="/create">
+        <Switch>
+          <Route exact path="/">
+            <Start />
+          </Route>
+          <Route exact path="/decentralised/create">
+            <LayoutDecentralised>
               <Create />
-            </Route>
-            <Route path="/stake">
+            </LayoutDecentralised>
+          </Route>
+          <Route exact path="/decentralised/stake">
+            <LayoutDecentralised>
               <Stake />
-            </Route>
-            <Route path="/exchange">
+            </LayoutDecentralised>
+          </Route>
+          <Route exact path="/decentralised/exchange">
+            <LayoutDecentralised>
               <Exchange />
-            </Route>
-            <Route exact path="/loan">
+            </LayoutDecentralised>
+          </Route>
+          <Route exact path="/decentralised/loan">
+            <LayoutDecentralised>
               <Loan />
-            </Route>
-            <Route exact path="/market">
+            </LayoutDecentralised>
+          </Route>
+          <Route exact path="/decentralised/market">
+            <LayoutDecentralised>
               <CollateralNfts />
-            </Route>
-            <Route path="/test">
-              <Test />
-            </Route>
-            <Route path="/">
-              
-            </Route>
-          </Switch>
-        </Layout>
+            </LayoutDecentralised>
+          </Route>
+          <Route exact path="/test">
+            <Test />
+          </Route>
+          <ThemeProvider theme={centerTheme}>
+            <LayoutCentralised>
+              <Route path="/centralised/create">
+                <CreateCentralised />
+              </Route>
+              <Route path="/centralised/stake">
+                <StakeCentralised />
+              </Route>
+              <Route path="/centralised/exchange">
+                <ExchangeCentralised />
+              </Route>
+              <Route path="/centralised/getjwt">
+                <GetJwt />
+              </Route>
+              <Route exact path="/centralised/submitkyc">
+                <SubmitKyc />
+              </Route>
+              <Route exact path="/centralised/loan">
+                <LoanCentralised />
+              </Route>
+              <Route exact path="/centralised/market">
+                <CollateralNftsCentralised />
+              </Route>
+            </LayoutCentralised>
+          </ThemeProvider>
+        </Switch>
       </Router>
     </ThemeProvider>
   );
--- src/signedContracts/signedC2.ts
+++ src/signedContracts/signedC2.ts
@@ -1,35 +1,24 @@
 import { ethers } from "ethers";
-import abiCrypto from "../contracts/decentralised/exchange.json";
-import abiNFT from "../contracts/decentralised/ExchangeNFT.json";
-import abiToken from "../contracts/decentralised/stakeToken.json";
+import abiCrypto from "../contracts/centralised/exchange.json";
+import abiNFT from "../contracts/centralised/ExchangeNFT.json";
+import abiToken from "../contracts/centralised/stakeToken.json";
 var hdate = require("human-date");
 
 const sepoliaURL = String(process.env.REACT_APP_SEPOLIA_URL);
 
-const networks = ["11155111", "80001"];
+const networks = ["11155111", "80001", "97"];
 const JRPCFromId = new Map([
   ["11155111", sepoliaURL],
   [
     "80001",
     "https://polygon-testnet.blastapi.io/2e3e0777-ba8f-4cf1-8f77-2aac489b3274",
   ],
+  ["97", "https://bsc-testnet.publicnode.com"],
 ]);
-const nameFromId = new Map([
-  ["11155111", "Sepolia"],
-  ["80001", "Mumbai"],
-  ["97", "BSC"],
-]);
-// "https://matic.getblock.io/04f401f9-44f5-4841-b934-71157c95af64/testnet/",
-
 const exchangeAddressFromId = new Map([
-  ["11155111", "0x00e91A750B5DD0237cF0A0623BB628C1B1724101"],
-  ["80001", "0x8F07078E70fe55316cDC106ef06A03Bc1fA8797F"],
-]);
-
-const networkIdInHex = new Map([
-  ["11155111", "0xaa36a7"],
-  ["80001", "0x13881"],
-  ["97", "0x61"],
+  ["11155111", "0xfC28ED155A3E23E6992eD3551c1822232FD2E9Ba"],
+  ["80001", "0x565ef94Bd04e988C6b15dCE6C47D5716C0DD36c4"],
+  ["97", "0x99528756Da1746779B34247fce6285d91A2c2868"],
 ]);
 
 function getSignedContract(id: string) {
@@ -52,6 +41,7 @@ async function getSignedNftContract(id: string) {
     destSigner
   );
   const nftContractAddress = await contract.exchangeNftAddr();
+
   const nftContract = new ethers.Contract(
     nftContractAddress!,
     abiNFT.abi,
@@ -68,7 +58,7 @@ async function getBalance(id: string) {
 const getNfts = async function (curAcc: string): Promise<any> {
   let nftContract;
   const myNfts = [];
-  for (let ct = 0; ct < 2; ct++) {
+  for (let ct = 0; ct < 3; ct++) {
     nftContract = await getSignedNftContract(networks[ct]);
     const count = Number(await nftContract?.totalSupply());
     for (let i = 0; i < count; i++) {
@@ -118,67 +108,79 @@ const getTokenContract = async function (chainId: string) {
   return tokenContract;
 };
 
-async function getCollateralNfts(chainId: string) {
-  const nfts: any = [];
-  const exchangeContract = getSignedContract(chainId);
-  const num = Number(await exchangeContract.usersCount());
-  console.log(num);
-  for (let i = 0; i < num; i++) {
-    const curUser = await exchangeContract.users(i);
-    const curUserLoan = await exchangeContract.loan(curUser);
-    console.log(curUserLoan);
-    const curTimestamp = Math.ceil(Number(Date.now()) / 1000);
-    const expiry = Number(curUserLoan.cutOffTimestamp);
-    const nftContract = await getSignedNftContract(chainId);
-    if (expiry < curTimestamp) {
-      const nftUriLoc = await nftContract.tokenURI(curUserLoan.nftTokenId);
-      let nftData = await fetch(nftUriLoc);
-      nftData = await nftData.json();
-      nfts.push({
-        ...nftData,
-        tokenId: i,
-        borrower: curUser,
-        price: curUserLoan.amount,
-      });
-    }
+async function getLoan(address: string) {
+  let loan;
+  let exchangeContract;
+  for (let ct = 0; ct < 3; ct++) {
+    exchangeContract = getSignedContract(networks[ct]);
+    loan = await exchangeContract.checkLoan(address);
+    if (loan.set)
+      return { ...loan, loanNetwork: networks[ct], borrower: address };
   }
-
-  return nfts;
+  return null;
 }
 
-async function getLoan(address: string, chainId: string) {
-  const exchangeContract = getSignedContract(chainId);
-  const loan = await exchangeContract.loan(address);
-  if (loan.set) return loan;
-  return null;
+async function getCollateralNfts() {
+  const nfts: any = [];
+
+  for (let ct = 0; ct < 3; ct++) {
+    const exchangeContract = getSignedContract(networks[ct]);
+    const num = Number(await exchangeContract.usersCount());
+    for (let i = 0; i < num; i++) {
+      const curUser = await exchangeContract.getUser(i);
+      const curUserLoan = await exchangeContract.checkLoan(curUser);
+      if (curUserLoan.nftChainId == "" || curUserLoan.collateralSold == true) {
+        continue;
+      }
+      const curTimestamp = Math.ceil(Number(Date.now()) / 1000);
+      const expiry = Number(curUserLoan.cutOffTimestamp);
+      const nftContract = await getSignedNftContract(curUserLoan.nftChainId);
+
+      if (expiry < curTimestamp) {
+        const nftUriLoc = await nftContract.tokenURI(curUserLoan.nftTokenId);
+        let nftData = await fetch(nftUriLoc);
+        nftData = await nftData.json();
+        nfts.push({
+          ...nftData,
+          tokenId: i,
+          network: curUserLoan.nftChainId,
+          price: curUserLoan.amount,
+          borrower: curUser,
+        });
+      }
+    }
+  }
+  console.log(nfts);
+  return nfts;
 }
 
 async function getAccountBalances(acc: string) {
   const balances = [];
-  for (let ct = 0; ct < 2; ct++) {
+  for (let ct = 0; ct < 3; ct++) {
     const tokenContract = await getTokenContract(networks[ct]);
     balances.push(Number(await tokenContract.balanceOf(acc)));
   }
   return balances;
 }
 
-const getStakes = async function (acc: string, network: string): Promise<any> {
-  const stakesArray: any = [];
-  let unlocked = 0;
-  // console.log(typeof network);
-  network = String(network);
-  const cryptoContract = getSignedContract(network);
-  const stakesNumber = await cryptoContract.stakesNumber(acc);
-  for (let i = 0; i < stakesNumber; i++) {
-    const thisStake = await cryptoContract.stakes(acc, i);
-    const timestamp = Number(thisStake[1]);
-    const value = String(thisStake[0]);
-    const relativeTime = timestamp - Math.floor(Date.now() / 1000);
-    if (relativeTime <= 0) {
-      unlocked += Number(value);
-    } else {
-      const prettyTime = hdate.prettyPrint(relativeTime, { showTime: true });
-      stakesArray.push({ time: prettyTime, value });
+const getStakes = async function (acc: string): Promise<any> {
+  const stakesArray = [];
+  let unlocked = [0, 0, 0];
+  for (let ct = 0; ct < 3; ct++) {
+    const cryptoContract = getSignedContract(networks[ct]);
+    const stakesNumber = await cryptoContract.stakesNumber(acc);
+    for (let i = 0; i < stakesNumber; i++) {
+      const thisStake = await cryptoContract.stakes(acc, i);
+      const timestamp = Number(thisStake[1]);
+      const value = String(thisStake[0]);
+
+      const relativeTime = timestamp - Math.floor(Date.now() / 1000);
+      if (relativeTime <= 0) {
+        unlocked[ct] += Number(value);
+      } else {
+        const prettyTime = hdate.prettyPrint(relativeTime, { showTime: true });
+        stakesArray.push({ time: prettyTime, value, network: networks[ct] });
+      }
     }
   }
   return [stakesArray, unlocked];
@@ -191,11 +193,9 @@ export {
   getSignedNftContract,
   getNfts,
   getNftAddress,
+  getLoan,
   getCollateralNfts,
   getAccountBalances,
-  getLoan,
   getStakes,
   networks,
-  nameFromId,
-  networkIdInHex,
 };
