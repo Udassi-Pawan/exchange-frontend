858fd30f81325361e8f4b4439380fd1f5c3d8351
Pawan Udassi <pawanudassi@hotmail.com>
2023-07-30 14:55:59 +0530

    added spinner and dialogue

--- src/App.tsx
+++ src/App.tsx
@@ -19,71 +19,124 @@ import StakeCentralised from "./pages/Centralised/Stake";
 import ExchangeCentralised from "./pages/Centralised/Exchange";
 import LoanCentralised from "./pages/Centralised/Loan";
 import centerTheme from "./MuiThemeCentralised";
+import { MyContext } from "./MyContext";
+import { useEffect, useState } from "react";
+import { Backdrop, CircularProgress } from "@mui/material";
+import MyDialogue from "./components/MyDialogue";
+import { ethers } from "ethers";
+import { exchangeAddressFromId } from "./signedContracts/scriptsCentralised";
+import abiExchangeCentralised from "./contracts/centralised/exchange.json";
 
 function App() {
+  const [acc, setAcc] = useState<string | null>(null);
+  const [chainId, setChainId] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean | null>(null);
+  const [dialogue, setDialogue] = useState<boolean | null>(null);
+  const [dialogueText, setDialogueText] = useState<string | null>("awf");
+
+  async function setCentralised() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = await provider.getSigner();
+    setAcc((await provider.listAccounts())[0]);
+    const { chainId } = await provider.getNetwork();
+    const chainIdString = String(chainId);
+    setChainId(chainIdString);
+    let contractAddress = exchangeAddressFromId.get(chainIdString);
+    const exchangeContract = new ethers.Contract(
+      contractAddress!,
+      abiExchange.abi,
+      signer
+    );
+    const nftContractAddress = await exchangeContract.exchangeNftAddr();
+    nftContract = new ethers.Contract(nftContractAddress!, abiNFT.abi, signer);
+  }
+  useEffect(() => {
+    setCentralised();
+  });
   return (
-    <ThemeProvider theme={mytheme}>
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Start />
-          </Route>
-          <Route exact path="/decentralised/create">
-            <LayoutDecentralised>
-              <Create />
-            </LayoutDecentralised>
-          </Route>
-          <Route exact path="/decentralised/stake">
-            <LayoutDecentralised>
-              <Stake />
-            </LayoutDecentralised>
-          </Route>
-          <Route exact path="/decentralised/exchange">
-            <LayoutDecentralised>
-              <Exchange />
-            </LayoutDecentralised>
-          </Route>
-          <Route exact path="/decentralised/loan">
-            <LayoutDecentralised>
-              <Loan />
-            </LayoutDecentralised>
-          </Route>
-          <Route exact path="/decentralised/market">
-            <LayoutDecentralised>
-              <CollateralNfts />
-            </LayoutDecentralised>
-          </Route>
-          <Route exact path="/test">
-            <Test />
-          </Route>
-          <ThemeProvider theme={centerTheme}>
-            <LayoutCentralised>
-              <Route path="/centralised/create">
-                <CreateCentralised />
-              </Route>
-              <Route path="/centralised/stake">
-                <StakeCentralised />
-              </Route>
-              <Route path="/centralised/exchange">
-                <ExchangeCentralised />
-              </Route>
-              <Route path="/centralised/getjwt">
-                <GetJwt />
-              </Route>
-              <Route exact path="/centralised/submitkyc">
-                <SubmitKyc />
-              </Route>
-              <Route exact path="/centralised/loan">
-                <LoanCentralised />
-              </Route>
-              <Route exact path="/centralised/market">
-                <CollateralNftsCentralised />
-              </Route>
-            </LayoutCentralised>
-          </ThemeProvider>
-        </Switch>
-      </Router>
-    </ThemeProvider>
+    <MyContext.Provider
+      value={{
+        acc,
+        setAcc,
+        setDialogue,
+        dialogue,
+        dialogueText,
+        setDialogueText,
+      }}
+    >
+      <ThemeProvider theme={mytheme}>
+        <Backdrop
+          sx={{
+            color: "#fff",
+            zIndex: (theme) => theme.zIndex.drawer + 1,
+          }}
+          open={loading! || dialogue!}
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop>
+        <MyDialogue />
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Start />
+            </Route>
+            <Route exact path="/decentralised/create">
+              <LayoutDecentralised>
+                <Create />
+              </LayoutDecentralised>
+            </Route>
+            <Route exact path="/decentralised/stake">
+              <LayoutDecentralised>
+                <Stake />
+              </LayoutDecentralised>
+            </Route>
+            <Route exact path="/decentralised/exchange">
+              <LayoutDecentralised>
+                <Exchange />
+              </LayoutDecentralised>
+            </Route>
+            <Route exact path="/decentralised/loan">
+              <LayoutDecentralised>
+                <Loan />
+              </LayoutDecentralised>
+            </Route>
+            <Route exact path="/decentralised/market">
+              <LayoutDecentralised>
+                <CollateralNfts />
+              </LayoutDecentralised>
+            </Route>
+            <Route exact path="/test">
+              <Test />
+            </Route>
+            <ThemeProvider theme={centerTheme}>
+              <LayoutCentralised>
+                <Route path="/centralised/create">
+                  <CreateCentralised />
+                </Route>
+                <Route path="/centralised/stake">
+                  <StakeCentralised />
+                </Route>
+                <Route path="/centralised/exchange">
+                  <ExchangeCentralised />
+                </Route>
+                <Route path="/centralised/getjwt">
+                  <GetJwt />
+                </Route>
+                <Route exact path="/centralised/submitkyc">
+                  <SubmitKyc />
+                </Route>
+                <Route exact path="/centralised/loan">
+                  <LoanCentralised />
+                </Route>
+                <Route exact path="/centralised/market">
+                  <CollateralNftsCentralised />
+                </Route>
+              </LayoutCentralised>
+            </ThemeProvider>
+          </Switch>
+        </Router>
+      </ThemeProvider>
+    </MyContext.Provider>
   );
 }
 
---
+++ src/components/MyDialogue.tsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
+import { TransitionProps } from "@mui/material/transitions";
+import { Box } from "@mui/material";
+import { MyContext } from "../MyContext";
+
+const Transition = React.forwardRef(function Transition(
+  props: TransitionProps & {
+    children: React.ReactElement<any, any>;
+  },
+  ref: React.Ref<unknown>
+) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+export default function MyDialogue() {
+  const { dialogue, dialogueText, setDialogue } = React.useContext(MyContext);
+  const handleClose = () => {
+    setDialogue(false);
+  };
+
+  return (
+    <Dialog
+      open={dialogue}
+      TransitionComponent={Transition}
+      keepMounted
+      onClose={handleClose}
+      aria-describedby="alert-dialog-slide-description"
+    >
+      <DialogTitle>{"Use Google's location service?"}</DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-slide-description">
+          {dialogueText}
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose}>Disagree</Button>
+        <Button onClick={handleClose}>Agree</Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
